dbbad80c066137cce38823b35aa556ca317b5685
brosenmoller <rosenmollerben@gmail.com>
2025-05-27 11:21:39 +0200

    Add new Debug Overloads

--- src/core/Debug.hpp
+++ src/core/Debug.hpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 class Debug 
 {
@@ -6,4 +11,14 @@ public:
 	static void Log(const char* message) {
 		std::cout << "[DEBUG]: " << message << std::endl;
 	}
+
+	static void Log(std::string message) {
+		std::cout << "[DEBUG]: " << message << std::endl;
+	}
+
+	static void Log(glm::vec2 value) { Log(glm::to_string(value)); }
+	static void Log(glm::vec3 value) { Log(glm::to_string(value)); }
+	static void Log(glm::mat3 value) { Log(glm::to_string(value)); }
+	static void Log(glm::mat4 value) { Log(glm::to_string(value)); }
+	static void Log(glm::quat value) { Log(glm::to_string(value)); }
 };
